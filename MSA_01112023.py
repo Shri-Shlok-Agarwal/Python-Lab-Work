9eed510a374d31a0f331fd87575acf0d7e8db67a
SHLOK AGARWAL <139161753+Shri-Shlok-Agarwal@users.noreply.github.com>
2023-11-01 15:37:29 +0530

    Add files via upload

---
+++ MSA_01112023.py
@@ -0,0 +1,39 @@
+#Date: 01/11/2023
+
+#_______________________________________________________________________________
+#WAP to create a dict using list1 elements asn key and list2 elements as value
+"""x=int(input("Enter number of Students"))
+list1=[]
+list2=[]
+dct={}
+
+for i in range (x):
+    a=input("Enter name of Student:")
+    b=int(input("Enter marks of Student:"))
+
+    list1+=[a]
+    list2+=[b]
+
+    dct[list1[i]]=list2[i]
+print(dct)"""
+
+        #or
+
+"""x=int(input("Enter number of Students"))
+list1=[]
+list2=[]
+dct={}
+
+print("Enter details of student")
+for i in range (x):
+    
+    list1.insert(i,input("Enter Name:"))
+    list2.insert(i,int(input("Enter Marks:")))
+    dct[list1[i]]=list2[i]
+print(dct)"""
+
+#__________________________________________________________________
+
+
+
+
